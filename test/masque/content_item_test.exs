c3f7d6d1d07ef707a655557ab1c5cc664ef6678d
Bronson Holden <bronson@bronsonholden.com>
2023-10-29 19:16:46 -0700

    Add tests for content item with remote schema

--- test/masque/content_item_test.exs
+++ test/masque/content_item_test.exs
@@ -5,31 +5,77 @@ defmodule Masque.ContentItemTest do
   alias Masque.Test.Repo
 
   setup do
-    {:ok, content_type} =
-      ContentType.new(%{name: "first_name", version: 1, schema: %{type: "string"}})
+    {:ok, people} =
+      ContentType.new(%{
+        name: "people",
+        version: 1,
+        schema: %{
+          "type" => "object",
+          "properties" => %{
+            "first_name" => %{"type" => "string"},
+            "last_name" => %{"type" => "string"}
+          }
+        }
+      })
       |> Repo.insert()
 
-    %{content_type: content_type}
+    {:ok, companies} =
+      ContentType.new(%{
+        name: "companies",
+        version: 1,
+        schema: %{
+          "type" => "object",
+          "properties" => %{
+            "name" => %{"type" => "string"},
+            "owner" => %{"$ref" => "/schemas/people/v1"}
+          }
+        }
+      })
+      |> Repo.insert()
+
+    %{people: people, companies: companies}
   end
 
   describe "create a ContentItem" do
-    test "with valid data", %{content_type: content_type} do
+    test "with valid data", %{people: people} do
       assert {:ok, content_item} =
-               ContentItem.new(content_type, %{data: "John"})
+               ContentItem.new(people, %{data: %{"first_name" => "John"}})
                |> Repo.insert()
 
       refute content_item.published_at
     end
 
-    test "with invalid attributes", %{content_type: content_type} do
-      assert {:error, _} =
-               ContentItem.new(content_type, %{data: 123})
+    test "with valid data for remote schema", %{companies: companies} do
+      assert {:ok, _} =
+               ContentItem.new(companies, %{
+                 data: %{
+                   "name" => "ACME, Inc.",
+                   "owner" => %{"first_name" => "Jane", "last_name" => "Roe"}
+                 }
+               })
                |> Repo.insert()
     end
 
-    test "as published", %{content_type: content_type} do
+    test "with invalid data", %{people: people} do
+      assert {:error, _} =
+               ContentItem.new(people, %{data: %{"first_name" => 123}})
+               |> Repo.insert()
+    end
+
+    test "with invalid data for remote schema", %{companies: companies} do
+      assert {:error, %Ecto.Changeset{}} =
+               ContentItem.new(companies, %{
+                 data: %{
+                   "name" => "ACME, Inc.",
+                   "owner" => %{"first_name" => 123, "last_name" => "Roe"}
+                 }
+               })
+               |> Repo.insert()
+    end
+
+    test "as published", %{people: people} do
       assert {:ok, content_item} =
-               ContentItem.new(content_type, %{data: "Jane"})
+               ContentItem.new(people, %{data: %{"first_name" => "Jane"}})
                |> ContentItem.published()
                |> Repo.insert()
 
